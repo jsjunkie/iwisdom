a5b0b5c78de95217d339e395ebe8bbf1e0564a55
Gulab Chopra <c.gulab@gmail.com>
2017-08-25 21:29:19 +0530

    make Wisdom editable based on propertiy

--- src/Wisdom.js
+++ src/Wisdom.js
@@ -3,16 +3,27 @@ import './Wisdom.css';
 import Lookup from './Lookup';
 
 class Wisdom extends Component {
+
 	render () {
+	  const span = this.props.editable ? '' : (
+		<span>{this.props.title}</span>	
+	  );
+
+	  const edit = this.props.editable ? (
+		<div>
+		<input type="text" value={this.props.title}></input>
+                <div className="improvewisdom">
+                  <textarea placeholder="Add description.."></textarea>
+                  <Lookup />
+                </div>
+		</div>
+		) : '';
+
 	  return (
 	    <div className="wisdom">
-		<span>{this.props.title}</span>
-		<input type="text" value={this.props.title}></input>
-		<div className="improvewisdom">
-                  <textarea placeholder="Add description.."></textarea>
-        	  <Lookup />    
-		</div>
-	    </div>
+		{span}
+	    	{edit}
+	     </div>
 	  )
 	}
 }

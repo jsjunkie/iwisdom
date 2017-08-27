74059fc96bc2ce28ed425797d45c57af628a203a
Gulab Chopra <c.gulab@gmail.com>
2017-08-27 16:16:24 +0530

    add save and home buttons

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
 	  { key: 1, title: "First", description: "Fer des", editable: false },
 	  { key: 2, title: "Second", description: "Se des", editable: false }
 	],
-	screen: 'main'
+	screen: 'main',
+	addWisdom : { title: '', description: ''}
     };
   }
 
@@ -58,18 +59,35 @@ class App extends Component {
 	this.setState({screen: 'main', wisdom: newWisdom});
    }
 
+   save (screen) {debugger;
+	if (screen === 'add' && this.state.addWisdom.title){
+	  var newWisdom = this.state.wisdom.slice();
+	   var addWisdom = this.state.addWisdom;
+	  newWisdom.push({title: addWisdom.title, description: addWisdom.description, editable: false});
+	  this.setState({wisdom: newWisdom, addWisdom: {title: '', description: ''}});
+	  //save State
+	  this.openBrowse();
+	} else {
+
+	}
+   }
+
   render() {
-    const home = this.state.screen === 'browse' ? (<div><button onClick={() => this.openHome()}>Home</button></div>) : '';
+    const buttons = (this.state.screen === 'browse' || this.state.screen === 'add') ? 
+			(<div>
+				<button onClick={() => this.save(this.state.screen)}>Save</button>
+				<button onClick={() => this.openHome()}>Home</button>
+			</div>) : '';
     const main = this.state.screen === 'main' ? <Main openAdd={() => this.openAdd()} openBrowse={() => this.openBrowse()}/> : '';
     const browse = this.state.screen === 'browse' ? <AllWisdom wisdom={this.state.wisdom} openEdit={(key) => this.openEdit(key)}/> : '';
-    const add = this.state.screen === 'add' ? <Wisdom editable="true" openEdit={() => {}}/> : '';
+    const add = this.state.screen === 'add' ? <Wisdom title={this.state.addWisdom.title} description={this.state.addWisdom.description} editable="true" openEdit={() => {}}/> : '';
     return (
       <div className="App">
         <div className="App-header">
           <div><img className="App-logo" src ={logo} alt="logo" onClick={() => this.openHome()}></img></div>
         </div>
 	<Search />
-	{home}
+	{buttons}
 	{main}
 	{browse}
 	{add}
